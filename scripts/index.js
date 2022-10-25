3d84758d8135c88b9113e3b796d77f57ae9a1fdb
Deepak <20bcs102@nith.ac.in>
2022-10-25 16:10:05 +0530

    Modified index.js

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+//NAVIGATION
+function myFunction() {
+var x = document.getElementById("myTopnav");
+if (x.className === "topnav") {
+x.className += " responsive";
+} else {
+x.className = "topnav";
+}
+}
